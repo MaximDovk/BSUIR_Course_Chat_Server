b8b0e9d586fb7d65eae454c32ba3532125f56d2c
MaximDovk <maximdovk@gmail.com>
2016-12-04 23:54:19 +0300

    Add show commands

--- src/main/java/me/shiftby/command/Interpreter.java
+++ src/main/java/me/shiftby/command/Interpreter.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class Interpreter {
 
     public Command interpret(String command, User sender) {
-        if (command.startsWith("/pm")) {
+        if (command.startsWith("/pm ")) {
             Pattern pattern = Pattern.compile("/pm\\s+(\\w+)\\s+(.*)");
             Matcher matcher = pattern.matcher(command);
             if (matcher.matches() && matcher.groupCount() == 2) {
@@ -16,7 +16,7 @@ public class Interpreter {
             } else {
                 return new InvalidArgumentsCommand(sender);
             }
-        } else if (command.startsWith("/gm")) {
+        } else if (command.startsWith("/gm ")) {
             Pattern pattern = Pattern.compile("/gm\\s+(\\w+)\\s+(.*)");
             Matcher matcher = pattern.matcher(command);
             if (matcher.matches() && matcher.groupCount() == 2) {
@@ -24,7 +24,7 @@ public class Interpreter {
             } else {
                 return new InvalidArgumentsCommand(sender);
             }
-        } else if (command.startsWith("/bm")) {
+        } else if (command.startsWith("/bm ")) {
             Pattern pattern = Pattern.compile("/bm\\s+(.*)");
             Matcher matcher = pattern.matcher(command);
             if (matcher.matches() && matcher.groupCount() == 1) {
@@ -32,7 +32,7 @@ public class Interpreter {
             } else {
                 return new InvalidArgumentsCommand(sender);
             }
-        } else if (command.startsWith("/gconnect")) {
+        } else if (command.startsWith("/gconnect ")) {
             Pattern pattern = Pattern.compile("/gconnect\\s+(.*)");
             Matcher matcher = pattern.matcher(command);
             if (matcher.matches() && matcher.groupCount() == 1) {
@@ -40,7 +40,7 @@ public class Interpreter {
             } else {
                 return new InvalidArgumentsCommand(sender);
             }
-        } else if (command.startsWith("/gdisconnect")) {
+        } else if (command.startsWith("/gdisconnect ")) {
             Pattern pattern = Pattern.compile("/gdisconnect\\s+(.*)");
             Matcher matcher = pattern.matcher(command);
             if (matcher.matches() && matcher.groupCount() == 1) {
@@ -48,6 +48,26 @@ public class Interpreter {
             } else {
                 return new InvalidArgumentsCommand(sender);
             }
+        } else if (command.startsWith("/gmembers ")) {
+            Pattern pattern = Pattern.compile("/gmembers\\s+(.*)");
+            Matcher matcher = pattern.matcher(command);
+            if (matcher.matches() && matcher.groupCount() == 1) {
+                return new GroupMembersCommand(sender, matcher.group(1));
+            } else {
+                return new InvalidArgumentsCommand(sender);
+            }
+        } else if (command.startsWith("/ugroups ")) {
+            Pattern pattern = Pattern.compile("/ugroups\\s+(.*)");
+            Matcher matcher = pattern.matcher(command);
+            if (matcher.matches() && matcher.groupCount() == 1) {
+                return new UserGroupsCommand(sender, matcher.group(1));
+            } else {
+                return new InvalidArgumentsCommand(sender);
+            }
+        } else if (command.equals("/gshow")) {
+            return new ShowGroupsCommand(sender);
+        } else if (command.equals("/ushow")) {
+            return new ShowUsersCommand(sender);
         } else if (command.equals("/disconnect")) {
             return new DisconnectCommand(sender);
         } else if (command.equals("/stop")) {
