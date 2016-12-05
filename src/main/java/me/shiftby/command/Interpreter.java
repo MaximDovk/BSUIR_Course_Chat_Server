f1845420ea348221d16a79eda4b9395a00481b60
MaximDovk <maximdovk@gmail.com>
2016-12-05 13:39:28 +0300

    Add config parsing and class filter

--- src/main/java/me/shiftby/command/Interpreter.java
+++ src/main/java/me/shiftby/command/Interpreter.java
@@ -1,79 +1,62 @@
 package me.shiftby.command;
 
+import javafx.util.Pair;
 import me.shiftby.entity.User;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Interpreter {
 
-    public Command interpret(String command, User sender) {
-        if (command.startsWith("/pm ")) {
-            Pattern pattern = Pattern.compile("/pm\\s+(\\w+)\\s+(.*)");
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches() && matcher.groupCount() == 2) {
-                return new PersonalMessageCommand(sender, matcher.group(1), matcher.group(2));
-            } else {
-                return new InvalidArgumentsCommand(sender);
-            }
-        } else if (command.startsWith("/gm ")) {
-            Pattern pattern = Pattern.compile("/gm\\s+(\\w+)\\s+(.*)");
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches() && matcher.groupCount() == 2) {
-                return new GroupMessageCommand(sender, matcher.group(1), matcher.group(2));
-            } else {
-                return new InvalidArgumentsCommand(sender);
-            }
-        } else if (command.startsWith("/bm ")) {
-            Pattern pattern = Pattern.compile("/bm\\s+(.*)");
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches() && matcher.groupCount() == 1) {
-                return new BroadcastCommand(sender, matcher.group(1));
-            } else {
-                return new InvalidArgumentsCommand(sender);
-            }
-        } else if (command.startsWith("/gconnect ")) {
-            Pattern pattern = Pattern.compile("/gconnect\\s+(.*)");
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches() && matcher.groupCount() == 1) {
-                return new GroupConnectCommand(sender, matcher.group(1));
-            } else {
-                return new InvalidArgumentsCommand(sender);
-            }
-        } else if (command.startsWith("/gdisconnect ")) {
-            Pattern pattern = Pattern.compile("/gdisconnect\\s+(.*)");
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches() && matcher.groupCount() == 1) {
-                return new GroupDisconnectCommand(sender, matcher.group(1));
-            } else {
-                return new InvalidArgumentsCommand(sender);
-            }
-        } else if (command.startsWith("/gmembers ")) {
-            Pattern pattern = Pattern.compile("/gmembers\\s+(.*)");
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches() && matcher.groupCount() == 1) {
-                return new GroupMembersCommand(sender, matcher.group(1));
-            } else {
-                return new InvalidArgumentsCommand(sender);
-            }
-        } else if (command.startsWith("/ugroups ")) {
-            Pattern pattern = Pattern.compile("/ugroups\\s+(.*)");
-            Matcher matcher = pattern.matcher(command);
-            if (matcher.matches() && matcher.groupCount() == 1) {
-                return new UserGroupsCommand(sender, matcher.group(1));
-            } else {
-                return new InvalidArgumentsCommand(sender);
-            }
-        } else if (command.equals("/gshow")) {
-            return new ShowGroupsCommand(sender);
-        } else if (command.equals("/ushow")) {
-            return new ShowUsersCommand(sender);
-        } else if (command.equals("/disconnect")) {
-            return new DisconnectCommand(sender);
-        } else if (command.equals("/stop")) {
-            return new ServerStopCommand();
+    private HashMap<Pattern, Class> commands;
+
+    public Interpreter(Properties properties) {
+        commands = parseProperties(properties);
+    }
+
+    private HashMap<Pattern, Class> parseProperties(Properties properties) {
+        HashMap<String, String> p = new HashMap<>();
+        for (String property : properties.stringPropertyNames()) {
+            p.put(property, properties.getProperty(property));
         }
-        return new InvalidCommand(sender);
+        HashMap<Pattern, Class> result = new HashMap<>();
+        p
+                .keySet()
+                .stream()
+                .filter(s -> s.startsWith("regexp."))
+                .map(s -> s.substring(7))
+                .filter(s -> p.get("class." + s) != null)
+                .forEach(s -> {
+                    try {
+                        Class c = Class.forName(p.get("class." + s));
+                        result.put(Pattern.compile(p.get("regexp." + s)), c);
+                    } catch (ClassNotFoundException ignored) {}
+                });
+        return result;
+    }
+
+
+    public Command interpret(String command, User sender)
+            throws NoSuchMethodException, IllegalAccessException,
+            InvocationTargetException, InstantiationException {
+        Pair<Pattern, Class> commandPair = commands
+                .keySet()
+                .parallelStream()
+                .filter(p -> {
+                    Matcher m = p.matcher(command);
+                    return m.matches();
+                })
+                .map(p -> new Pair<>(p, commands.get(p)))
+                .findAny()
+                .orElse(new Pair<>(Pattern.compile(""), InvalidCommand.class));
+
+        return (Command) commandPair
+                .getValue()
+                .getConstructor(User.class, String.class, Pattern.class)
+                .newInstance(sender, command, commandPair.getKey());
     }
 
 }
