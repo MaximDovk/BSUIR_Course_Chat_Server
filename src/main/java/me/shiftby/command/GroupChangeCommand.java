306d8a693bbfe9c5fc6c3226135e73d61b1f579b
MaximDovk <maximdovk@gmail.com>
2016-12-05 18:54:25 +0300

    Create group change command

---
+++ src/main/java/me/shiftby/command/GroupChangeCommand.java
@@ -0,0 +1,39 @@
+package me.shiftby.command;
+
+import me.shiftby.Main;
+import me.shiftby.entity.Group;
+import me.shiftby.entity.User;
+import me.shiftby.orm.GroupManager;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GroupChangeCommand implements Command {
+
+    private User from;
+    private String group;
+    private String key;
+    private String value;
+
+    public GroupChangeCommand(User from, String command, Pattern pattern) {
+        this.from = from;
+        Matcher m = pattern.matcher(command);
+        m.matches();
+        group = m.group(1);
+        key = m.group(2);
+        value = m.group(3);
+    }
+
+    @Override
+    public void execute() throws IOException {
+        Group g = GroupManager.getInstance().findByName(group);
+        if (g != null) {
+            switch (key) {
+            }
+            Main.getSessionManager().getByUsername(from.getUsername()).send("status.group.changed");
+        } else {
+            Main.getSessionManager().getByUsername(from.getUsername()).send("status.group.invalid");
+        }
+    }
+}
